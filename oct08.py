a10dae17720112531f971c33ebd7096f1ad1a8a8
anayeli ochoa <ochoa@example.com>
2019-10-08 21:11:15 -0600

    class

---
+++ oct08.py
@@ -0,0 +1,39 @@
+#%%
+''' w es write(creates ir overwrites) y r es read
+a appends
+'''
+filename = open("text.txt", "w")#aqui los files no van indented
+# always close your file
+filename.close()
+#el siguiente es otra forma de abrir file
+with open("text.txt", "w")
+#esto abre y cierra y los statements van indentados
+
+
+
+#%%
+'''
+tyoe ls en el terminal de jupiter
+luego
+ls -l
+luego
+ls -l > text.txt
+esto > write or creates file
+esto >> appends to a file
+luego pon cat test.txt
+-----
+trabaje hoy en jupiter
+split separa cuando vve espacios y se convirtio
+ en una lista de strings y asi podemos ver por index
+----------
+tmb se puede readlines
+with open("test.txt", "r) as in file
+lines = infile.readlines()
+lines = lines[1:]
+print(lines)
+Jupyter Server URI: http://localhost:8080/?token=c28a9df86c224811bafc2c88a7fe236e
+#%%
+
+python de dicmebre
+Jupyter Server URI: http://localhost:8890/?token=a16ea7cf9109a096e4db65a5e6309174e8baf28052a3f33d
+Python version:
--- seriesPrompt.py
+++ seriesPrompt.py
@@ -1,5 +1,19 @@
+# %%
+n = int(input("Enter an integer value: "))
+
+print("You entered: ", n)
+
+x = range(1, n+1)
+for j in x:
+  print(j)
+  formula = j + 2
+  print(formula, "formula inside for loop")
+
+
+# %%
+
+f = 1 + (1/2) + (1/2) + (1/2)
+print(f)
+
+
 #%%
-'''
-'''
-C = raw_input('C=? ')
-print(C)
