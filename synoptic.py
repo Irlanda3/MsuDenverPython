6609840d5a7841cb857eeb6f3d99b5de2f08a569
anayeli ochoa <ochoa@example.com>
2019-10-27 23:59:52 -0600

    gat

--- synoptic.py
+++ synoptic.py
@@ -6,47 +6,35 @@ import matplotlib.pyplot as plt
 
 
 def gatFnc(phi_degrees):
-    omega = (2*pi)/86164.09
-
-
+    omega = (2*pi)/86164
     print("Earth angular velocity: ", omega)
     # f_coriolisParameter -> earth local rotatiom -> f = 2*omega*sin(phi_degrees)
     #phi_degrees = pi/6  # 30degrees
     math.degrees(phi_degrees)
     f = 2*omega*sin(phi_degrees)
     math.degrees(f)
-    print("f_coriolis parameter: ", f)  # we got a float
-    print(type(f))
+    print("f_coriolis parameter: ", f)  # we got a float    
     dosTimesPi = 2*pi
     geostrophic_adjustmentTime = (0.25)*((dosTimesPi) / f)
-
     print("Geostrophic Adjustment Time: ", geostrophic_adjustmentTime)
-    
+    print("")
 
     return geostrophic_adjustmentTime
 
-plt.plot(gatFnc(pi/6), 'g^')
-plt.ylabel("GAT")
+degreeName = [30,35,40,45,50,55,60]
+
+gatList = []
+gatList = (gatFnc(pi/6),gatFnc(7*pi/36),gatFnc(2*pi/9),gatFnc(pi/4),gatFnc(5*pi/18),gatFnc(11*pi/36),gatFnc(pi/3))
+plt.plot(gatList, 'g^')#talves sea buena idea poner en una lista todos los resultados del gat
+
+plt.ylabel("GAT TIME")
+plt.xlabel("LATITUDES")
+
+plt.xticks([0, 1,2,3,4, 5,6], ('x = 30','x = 35','x = 40','x = 45','x = 50','x = 55','x = 60'))
+#plt.tight_layout()
+
+plt.title("Geostrophic Adjustment Time")
+plt.grid(True)
 plt.show()
-
-# gatFnc(pi/6)
-# gatFnc(7*pi/36)
-# name = ['30deg']
-# plt.title('Geostrophic Adjustment Time!')
-
-# plt.plot(gatFnc(pi/6),name)
-# plt.show()
-
-# the histogram of the data
-# n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
-
-
-# plt.xlabel('Smarts')
-# plt.ylabel('Probability')
-# plt.title('Histogram of IQ')
-# plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
-# plt.axis([40, 160, 0, 0.03])
-# plt.grid(True)
-# plt.show()
 
 # %%
