8345ed7e192b86b44d553b949b80d1ca0461b618
anayeli ochoa <ochoa@example.com>
2019-10-31 10:48:15 -0600

    cont.

--- halloweenDay.py
+++ halloweenDay.py
@@ -16,10 +16,58 @@ y1 = np.sin(x)  # le hacemos el sin de ese array
 y2 = np.cos(x)
 
 plt.plot(x, y1, label="Sine Function")
+plt.plot(x, y2, label="Cosine Function")
 plt.legend()
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.title("Our first plot(s)")
+plt.show()# este siempre debe ir al ultimo para q grafique todo
+
+# %%
+from matplotlib import pyplot as plt
+import numpy as np  # para poder usar linspace
+from math import exp
+
+fnc = x**2*exp(-x**2)#from 0 to 4
+plt.plot(x, label="Exponential function")
+plt.legend()
 plt.show()
+
+# %%
+from matplotlib import pyplot as plt
+import numpy as np  # para poder usar linspace
+from math import exp
+
+x = np.linspace(0,4,65)# el 65 representa cuanto snumeros quieres
+y = x**2*np.exp(-x**2)
+plt.plot(x,y, label = "x^2 e^(-x^2)", marker = "o", linestyle = "None")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Nice Function")
+plt.xlim(0,3)#tmb podemos decir xmax=3, xmin=0
+plt.show()
+'''podemos usar help(plt)
+label es un keyword argument, x and y son positional
+'''
+
+# %%
+from matplotlib import pyplot as plt
+import numpy as np  # para poder usar linspace
+from math import exp
+
+x = np.linspace(0,4,65)# el 65 representa cuanto snumeros quieres
+y = x**2*np.exp(-x**2)
+plt.plot(x,y, "r-.", label = "x^2 e^(-x^2)",)
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Nice Function")
+plt.xlim(0,3)#tmb podemos decir xmax=3, xmin=0
+plt.show()
+'''podemos usar help(plt)
+label es un keyword argument, x and y son positional
+'''
+
 
 # %%
